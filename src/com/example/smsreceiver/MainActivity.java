3b76a7a5d85fbe630f869133d399d7058229e8fe
alchemist_d <alchemist_d@cb0e805b-d4c5-4afa-be9c-75694c837024>
2013-04-10 18:20:11 +0000

    SMSReceiver만듬. 쓸데없이 Notification 해볼꺼라고 찾아댕기다가 잠 못잠.

--- src/com/example/smsreceiver/MainActivity.java
+++ src/com/example/smsreceiver/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.smsreceiver;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	public String NOTIFICATION_DATA = "NOTIFICATION_DATA";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +28,40 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	/* Notification Bulider는 API 11부터라 호환되는 library를 써야함... PAINFUL ㅡㅡ */
+	public void createNotification(long when,String data){
+		String notificationContent ="Notification Content Click Here to go more details";
+		String notificationTitle ="Title";
+		
+		Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher);
+		int smalIcon =R.drawable.ic_launcher;
+		String notificationData="This is data : "+ data;
+
+		Intent intent =new Intent(getApplicationContext(), NotificationDetailsActivity.class);
+		intent.putExtra(NOTIFICATION_DATA, notificationData);
+
+		intent.setData(Uri.parse("content://"+when));
+		PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, Intent.FLAG_ACTIVITY_NEW_TASK);
+
+		NotificationManager notificationManager =(NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE); 
+
+		NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(
+				getApplicationContext())
+				.setWhen(when)
+				.setContentText(notificationContent)
+				.setContentTitle(notificationTitle)
+				.setSmallIcon(smalIcon)
+				.setAutoCancel(true)
+				.setTicker(notificationTitle)
+				.setLargeIcon(largeIcon)
+				.setDefaults(Notification.DEFAULT_LIGHTS| Notification.DEFAULT_VIBRATE| Notification.DEFAULT_SOUND)
+				.setContentIntent(pendingIntent);
+
+		Notification notification=notificationBuilder.build();
+
+		notificationManager.notify((int) when, notification);
 	}
 
 }

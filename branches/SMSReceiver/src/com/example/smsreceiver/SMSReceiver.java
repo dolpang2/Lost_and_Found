d16b472838c0daacc4051158526505f33ff20f4b
alchemist_d <alchemist_d@cb0e805b-d4c5-4afa-be9c-75694c837024>
2013-04-10 18:20:11 +0000

    SMSReceiver만듬. 쓸데없이 Notification 해볼꺼라고 찾아댕기다가 잠 못잠.

---
+++ branches/SMSReceiver/src/com/example/smsreceiver/SMSReceiver.java
@@ -0,0 +1,38 @@
+package com.example.smsreceiver;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.content.BroadcastReceiver;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class SMSReceiver extends BroadcastReceiver {
+	public static final String NOTIFICATION_DATA = "NOTIFICATION_DATA";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Bundle extras = intent.getExtras();
+		
+		if (extras != null) {
+			Object[] pdus = (Object[]) extras.get("pdus");
+			SmsMessage[] messages = new SmsMessage[pdus.length];
+
+			for (int i = 0; i < pdus.length; i++) {
+				messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+			}
+
+			for (SmsMessage message : messages) {
+				String msg = message.getMessageBody();
+				// String from = message.getOriginatingAddress();
+				if (msg.startsWith("@")) {
+					Toast.makeText(context, "Special Message!", Toast.LENGTH_SHORT).show();
+					Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(context, "usual message", Toast.LENGTH_LONG).show();
+				}
+			}
+		}
+	}
+
+}

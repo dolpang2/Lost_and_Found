a3fbfc84781315af9ea3f58a3773485f9171e161
alchemist_d <alchemist_d@cb0e805b-d4c5-4afa-be9c-75694c837024>
2013-04-11 11:46:10 +0000

    [#53978] SMSReceiver issue end

--- branches/SMSReceiver/src/com/example/smsreceiver/MainActivity.java
+++ branches/SMSReceiver/src/com/example/smsreceiver/MainActivity.java
@@ -15,43 +15,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
-	/* Notification Bulider는 API 11부터라 호환되는 library를 써야함... PAINFUL
-	public void createNotification(long when,String data){
-		String notificationContent ="Notification Content Click Here to go more details";
-		String notificationTitle ="Title";
-		
-		Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher);
-		int smalIcon =R.drawable.ic_launcher;
-		String notificationData="This is data : "+ data;
-
-		Intent intent =new Intent(getApplicationContext(), NotificationDetailsActivity.class);
-		intent.putExtra(NOTIFICATION_DATA, notificationData);
-
-		intent.setData(Uri.parse("content://"+when));
-		PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, Intent.FLAG_ACTIVITY_NEW_TASK);
-
-		NotificationManager notificationManager =(NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE); 
-
-		NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(
-				getApplicationContext())
-				.setWhen(when)
-				.setContentText(notificationContent)
-				.setContentTitle(notificationTitle)
-				.setSmallIcon(smalIcon)
-				.setAutoCancel(true)
-				.setTicker(notificationTitle)
-				.setLargeIcon(largeIcon)
-				.setDefaults(Notification.DEFAULT_LIGHTS| Notification.DEFAULT_VIBRATE| Notification.DEFAULT_SOUND)
-				.setContentIntent(pendingIntent);
-
-		Notification notification=notificationBuilder.build();
-
-		notificationManager.notify((int) when, notification);
-	}
-	*/
 }

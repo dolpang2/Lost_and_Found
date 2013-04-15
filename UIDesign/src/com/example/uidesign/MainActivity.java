26d9c795b8862d71049d478546bc9cea81fce829
ch0645 <ch0645@cb0e805b-d4c5-4afa-be9c-75694c837024>
2013-04-15 03:33:08 +0000

    SubActivity

--- UIDesign/src/com/example/uidesign/MainActivity.java
+++ UIDesign/src/com/example/uidesign/MainActivity.java
@@ -1,23 +1,65 @@
 package com.example.uidesign;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+  public Button settingButton;
+  public Button changeEmailButton;
+  public Button changePassword;
+  public Button supportButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    settingButton = (Button)findViewById(R.id.action_settings);
+    changeEmailButton = (Button)findViewById(R.id.changeRegisteredEmail);
+    changePassword = (Button)findViewById(R.id.changePassword);
+    supportButton = (Button)findViewById(R.id.support);
     
+    findViewById(R.id.action_settings).setOnClickListener(mClickListener);
+    findViewById(R.id.changeRegisteredEmail).setOnClickListener(mClickListener);
+    findViewById(R.id.changePassword).setOnClickListener(mClickListener);
+    findViewById(R.id.support).setOnClickListener(mClickListener);
+
+  }
+
+  Button.OnClickListener mClickListener = new View.OnClickListener() {
+
+    @Override
+    public void onClick(View v) {
+      // TODO Auto-generated method stub
+
+      switch (v.getId()) {
+      case R.id.action_settings:
+        settingButton.setOnClickListener(new Button.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            // TODO Auto-generated method stub
+            Intent intent 
+            = new Intent(MainActivity.this, SupportActivity.class);
+            
+            startActivity(intent);
+          }
+        });
+        break;
+      default:
+        break;
+      }
+    }
+  };
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
 }

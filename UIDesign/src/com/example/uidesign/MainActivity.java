3b2f23e1499bdd69201342f7f8eea05f71d41b41
ch0645 <ch0645@cb0e805b-d4c5-4afa-be9c-75694c837024>
2013-04-15 03:53:36 +0000

    MainActivity Exist Not SubActivity

--- UIDesign/src/com/example/uidesign/MainActivity.java
+++ UIDesign/src/com/example/uidesign/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.uidesign;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -23,37 +21,8 @@ public class MainActivity extends Activity {
     changePassword = (Button)findViewById(R.id.changePassword);
     supportButton = (Button)findViewById(R.id.support);
     
-    findViewById(R.id.action_settings).setOnClickListener(mClickListener);
-    findViewById(R.id.changeRegisteredEmail).setOnClickListener(mClickListener);
-    findViewById(R.id.changePassword).setOnClickListener(mClickListener);
-    findViewById(R.id.support).setOnClickListener(mClickListener);
 
   }
-
-  Button.OnClickListener mClickListener = new View.OnClickListener() {
-
-    @Override
-    public void onClick(View v) {
-      // TODO Auto-generated method stub
-
-      switch (v.getId()) {
-      case R.id.action_settings:
-        settingButton.setOnClickListener(new Button.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-            // TODO Auto-generated method stub
-            Intent intent 
-            = new Intent(MainActivity.this, SupportActivity.class);
-            
-            startActivity(intent);
-          }
-        });
-        break;
-      default:
-        break;
-      }
-    }
-  };
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {

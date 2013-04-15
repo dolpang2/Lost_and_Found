55909673a62e9ea8b86f921d1ace3f6e5ff43d54
ch0645 <ch0645@cb0e805b-d4c5-4afa-be9c-75694c837024>
2013-04-15 08:17:30 +0000

    1. MainActivity's connect implementation 2. Create SettingActivity

--- branches/UIDesign/src/com/example/uidesign/MainActivity.java
+++ branches/UIDesign/src/com/example/uidesign/MainActivity.java
@@ -1,22 +1,54 @@
 package com.example.uidesign;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	public Button settingButton;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.main, menu);
-    return true;
-  }
+		settingButton = (Button)findViewById(R.id.settingButton);
+
+		findViewById(R.id.settingButton).setOnClickListener(mClickListener);
+	}
+
+	Button.OnClickListener mClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+			switch (v.getId()) {
+			case R.id.settingButton:
+				settingButton.setOnClickListener(new Button.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+						startActivity(intent);
+					}
+				});
+				break;
+
+			default:
+				break;
+			}
+
+		}
+	};
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }

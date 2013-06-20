1cdb3cf90c70d6668e3e8193224be60038c2b873
alchemist_d <alchemist_d@cb0e805b-d4c5-4afa-be9c-75694c837024>
2013-06-20 07:42:13 +0000

    [#53993]Little Change 1

--- LostFound/src/kr/lee/lostfound/ContactsService.java
+++ LostFound/src/kr/lee/lostfound/ContactsService.java
@@ -1,29 +1,14 @@
 package kr.lee.lostfound;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.app.Service;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.IBinder;

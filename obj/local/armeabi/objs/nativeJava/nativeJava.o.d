188c1f2e884784ff834760a0ef3cdb88aa76f558
ch0645 <ch0645@cb0e805b-d4c5-4afa-be9c-75694c837024>
2013-06-20 17:10:28 +0000

    JNI Module

---
+++ obj/local/armeabi/objs/nativeJava/nativeJava.o.d
@@ -0,0 +1,22 @@
+obj/local/armeabi/objs/nativeJava/nativeJava.o: jni/nativeJava.c \
+ /home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/string.h \
+ /home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/jni.h \
+ jni/nativeJava.h
+
+/home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/string.h:
+
+/home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/home/Lee/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/jni.h:
+
+jni/nativeJava.h:
